7cc8e5c8efbdef7fe1f6f3e4cd24891ce339e23f
lEEahht <lht8423@nyu.edu>
2025-02-27 13:04:57 -0500

    Update Q3_project.v

--- src/Q3_project.v
+++ src/Q3_project.v
@@ -2,12 +2,13 @@
  * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
-
 `default_nettype none
+`timescale 1ns / 1ps
 
 module tt_um_Q3_project (
     input wire clk,          // Clock input
     input wire rst_n,        // Active low reset
+    input wire ena,          // Enable signal
     input wire [7:0] ui_in,  // Input A
     input wire [7:0] uio_in, // Input B
     output reg [7:0] uo_out, // Output C
@@ -25,11 +26,11 @@ module tt_um_Q3_project (
     assign uio_out = uo_out;
     assign uio_oe = 8'b11111111;
 
-    // Sequential Logic for Output with Reset
+    // Sequential Logic for Output with Reset and Enable
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             uo_out <= 8'b00000000;
-        end else begin
+        end else if (ena) begin
             // Assign MUX result to uo_out[6:0]
             uo_out[6:0] <= mux_result;
             // uo_out[7] is AND of ui_in[7] and uio_in[7]
